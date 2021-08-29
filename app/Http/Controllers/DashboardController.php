8930224cf2554bc628f2121f2356854a4fe344bd
DemionNeo <leonnel230@gmail.com>
2021-08-29 07:52:38 +0300

    fisrt commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index() 
+    {
+        if(Auth::user()->hasRole('user')){
+            return view('userdas');
+        }elseif(Auth::user()->hasRole('trainer')){
+            return view('trainerdahs');
+        }elseif(Auth::user()->hasRole('admin')){
+            return view('dashboar');
+        }
+    }
+}
